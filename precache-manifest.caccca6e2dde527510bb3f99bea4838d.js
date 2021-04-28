e6d5b0698b08a53a82ceba77b644588e3c37f1cb
adir abargil <adir1661@users.noreply.github.com>
2021-04-28 13:14:00 +0300

    Updates

---
+++ precache-manifest.caccca6e2dde527510bb3f99bea4838d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c412b02110209a7b7f3d5f057626a9cd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fb169eb5be76f5bd9eca",
+    "url": "./static/css/main.d22fdc01.chunk.css"
+  },
+  {
+    "revision": "19c8e2e8a6168a56ddc8",
+    "url": "./static/js/2.0ce5ae05.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "./static/js/2.0ce5ae05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb169eb5be76f5bd9eca",
+    "url": "./static/js/main.13a0b06f.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.13a0b06f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92b51876bdc5af6968b6",
+    "url": "./static/js/runtime-main.ab08a126.js"
+  }
+]);
